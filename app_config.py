6c8108588fbf70649e58a49a86f4cd22f879c65e
thetisrock <torok.chris@gmail.com>
2018-11-20 17:19:19 -0500

    "Added wsgi entry point & app config info plus ignore list"

---
+++ app_config.py
@@ -0,0 +1,11 @@
+#
+#   This file deals with Flask App configuration details. Ideally, all configuration details will go here. 
+#
+from flask import Flask
+
+###################################################################################################
+######################################## APPLICATION CONFIGURATION ################################
+###################################################################################################
+
+app = Flask(__name__)  # create flask object. object has underlying configuration capabilities.
+app.secret_key = 'autobuddy_session_key'  # set a key for session cookie signing.
