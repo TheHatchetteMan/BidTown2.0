4086da593caa35615343ee40f40e180d9d01eac0
Erin <etwitty@email.uscupstate.edu>
2018-11-24 12:36:15 -0500

    added a for loop to view top ten items

--- routes.py
+++ routes.py
@@ -6,14 +6,31 @@ from DB_Helper import DB_Helper
 
 @app.route("/")
 def base():
-    return render_template("base.html")
+	return render_template("base.html")
 	
 #------------------------------------- HOME -------------------------------------#
 @app.route('/')
 @app.route('/HomePage')
-def HomePage():
-    return render_template('HomePage.html')
+def view_popular_item():
+    db = DB_Helper()
+    sql = ('SELECT ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date '
+           'FROM Item ORDER BY Bid_Count LIMIT 10')
 
+    empty_tuple = ()
+
+    cursor = db.connection.cursor(prepared=True)
+    cursor.execute(sql, empty_tuple)
+
+    results = cursor.fetchall()
+
+    item_list = {}
+    item_list['item'] = []
+
+    for (ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date) in results:
+        item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Current_Bid.decode(), Bid_Count, Start_Date, End_Date])
+
+    db.disconnect()
+    return render_template("HomePage.html", item_list=item_list)
 #------------------------------------- CREATE ACCOUNT -------------------------------------#
 @app.route('/')
 @app.route('/HomePage')
@@ -31,38 +48,38 @@ def Login():
 #------------------------------------ Test Page----------------------------------#
 @app.route("/single-item")  # testing
 def view_single_item():
-    db = DB_Helper()
-    sql = ("SELECT ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date " \
-          "FROM Item")
-    no_data = ()  # for prepared statement functionality
-    cursor = db.connection.cursor(prepared=True)
-    cursor.execute(sql, no_data)
+	db = DB_Helper()
+	sql = ("SELECT ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date " \
+		  "FROM Item")
+	no_data = ()  # for prepared statement functionality
+	cursor = db.connection.cursor(prepared=True)
+	cursor.execute(sql, no_data)
 
-    results = cursor.fetchall()
+	results = cursor.fetchall()
 
-    item_data = {}
+	item_data = {}
 
-    for (ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date) in results:
-        item_data['item'] = [ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Current_Bid.decode(), Bid_Count, Start_Date, End_Date]
+	for (ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date) in results:
+		item_data['item'] = [ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Current_Bid.decode(), Bid_Count, Start_Date, End_Date]
 
-    db.disconnect(commit=True)
-    return render_template("ItemForm.html", item_data=item_data)
+	db.disconnect(commit=True)
+	return render_template("ItemForm.html", item_data=item_data)
 
 @app.route("/place-bid", methods=['POST'])
 def place_bid():
-    if request.method == 'POST' and (request.form != None) or len(request.form) != 0:
-        bid = request.form['bid']
-        Item_ID = request.form['item-id']
-        expected_bidcount = request.form['expected-bidcount']
+	if request.method == 'POST' and (request.form != None) or len(request.form) != 0:
+		bid = request.form['bid']
+		Item_ID = request.form['item-id']
+		expected_bidcount = request.form['expected-bidcount']
 
-        db = DB_Helper()
-        sql = ("UPDATE Item "
-               "SET Current_Bid = Current_Bid + ?, Bid_Count = Bid_Count + 1 "
-               f"WHERE ItemID = ? AND Bid_Count = {expected_bidcount}"
-               )
+		db = DB_Helper()
+		sql = ("UPDATE Item "
+			   "SET Current_Bid = Current_Bid + ?, Bid_Count = Bid_Count + 1 "
+			   f"WHERE ItemID = ? AND Bid_Count = {expected_bidcount}"
+			   )
 
-        update = db.connection.cursor(prepared=True)
-        update.execute(sql, (bid, Item_ID,))
-        db.disconnect()
-        return redirect("/single-item")
-    return "Error"
+		update = db.connection.cursor(prepared=True)
+		update.execute(sql, (bid, Item_ID,))
+		db.disconnect()
+		return redirect("/single-item")
+	return "Error"
