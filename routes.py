3c3335b098cc8f163c96633179849a8d0991cf5e
Christopher Torok <torok.chris@gmail.com>
2018-11-24 19:41:20 -0500

    Added BidManager class, encapsulated place_bid in BidManager method
    place_bid functionality encapsulated within BidManager object.
    Note: future functionality of the BidManager class may include the state of the current bid being request. This way data can be passed to different URLs without being lost and without supplying potentially sensitive information through HTML forms.

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from app_config import app
 from flask import render_template, request, redirect, url_for, flash
 from DB_Helper import DB_Helper
+from BidManager import BidManager
+
+# static
+bm = BidManager()
 
 # @app.context_processor  # use this for loading base.html layout with common elements among views
 
@@ -29,12 +33,13 @@ def Login():
 	return render_template('LoginForm.html')
 
 #------------------------------------ Test Page----------------------------------#
-@app.route("/single-item")  # testing
-def view_single_item():
+@app.route("/single-item/<int:ItemID>")  # testing
+def view_single_item(ItemID):
     db = DB_Helper()
-    sql = ("SELECT ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date " \
-          "FROM Item")
-    no_data = ()  # for prepared statement functionality
+    sql = ("SELECT ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date "
+           "FROM Item "
+           "WHERE ItemID={ItemID}")
+    no_data = ()  # for prepared statement functionality. not sure if this is required.
     cursor = db.connection.cursor(prepared=True)
     cursor.execute(sql, no_data)
 
@@ -43,48 +48,16 @@ def view_single_item():
     item_data = {}
 
     for (ItemID, UserID, ClassID, Name, Image_Url, Status, Current_Bid, Bid_Count, Start_Date, End_Date) in results:
-        item_data['item'] = [ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Current_Bid.decode(), Bid_Count, Start_Date, End_Date]
+        item_data['item'] = [ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Current_Bid.decode(),
+                             Bid_Count, Start_Date, End_Date]
+
+    bm.single_item_result = item_data['item'][0]  # store a list of attributes of a single item
 
     db.disconnect(commit=True)
     return render_template("ItemForm.html", item_data=item_data)
 
-@app.route("/place-bid", methods=['POST'])
+@app.route("/place-bid", methods=['POST'])  # executed on ItemForm.html view
 def place_bid():
     if request.method == 'POST' and (request.form is not None) or len(request.form) != 0:
-        db = DB_Helper()
-
-        # data
-        bid_increment = float(request.form['bid'])
-        Item_ID = request.form['item-id']
-        expected_bidcount = int(request.form['expected-bidcount'])
-        expected_bid_total = float(request.form['expected-bid']) + bid_increment
-
-        # check that bid expectation is what will actually happen AKA recheck for actual bid and bid count match
-        cursor = db.connection.cursor()
-        cursor.execute(f"SELECT Current_Bid, Bid_Count FROM Item WHERE ItemID={Item_ID}")
-        actual_bidcount = -1
-        actual_current_bid = bid_increment
-        for (Current_Bid, Bid_Count) in cursor:
-            actual_bidcount = Bid_Count
-            actual_current_bid = float(Current_Bid)
-        cursor.close()
-
-        actual_bid_total = actual_current_bid + bid_increment
-
-        meets_count = expected_bidcount == actual_bidcount  # is the user behind in their attempt to bid
-        meets_expected_bid = expected_bid_total == actual_bid_total  # does the user's expected bid match what will be updated
-
-        if meets_count and meets_expected_bid:
-            sql = ("UPDATE Item "
-                   "SET Current_Bid = Current_Bid + ?, Bid_Count = Bid_Count + 1 "
-                   f"WHERE ItemID = ? AND Bid_Count = {actual_bidcount} "
-                   "AND Current_Bid < (Current_Bid + ?) "  # adding negative & zero amounts
-                   )  # check bid count at database level
-            # cursor.close()
-            update = db.connection.cursor(prepared=True)
-            update.execute(sql, (bid_increment, Item_ID, expected_bidcount,))
-            db.disconnect()
-            return "Success"
-        else:
-            db.disconnect()
-            return "You snooze, you lose! Someone else placed a bid while you were looking at the item"
+        return bm.place_bid()
+    return "error"
