7e77fd447776ed13c7eb342d87e88e2c7f21b6be
Christopher Torok <torok.chris@gmail.com>
2018-12-01 04:30:53 -0500

    added comment

--- routes.py
+++ routes.py
@@ -221,7 +221,7 @@ def authenticate():
 #------------------------------------- CREATE ACCOUNT -------------------------------------#
 @app.route('/CreateAccount', methods=['POST', 'GET'])
 def CreateAccount():
-    if 'bidtown_session_key' in session and session['bidtown_session_key'] is not None:  # fixed key error here
+    if 'bidtown_session_key' in session and session['bidtown_session_key'] is not None:  # fixed key error here - Chris
         return redirect('/HomePage')
     if request.method == 'POST':
         userEmail = request.form['email']
