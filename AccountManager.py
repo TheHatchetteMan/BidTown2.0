c0f8ffdffcc4955c8e715fce4164c1ff1e53cfd8
Hunter Hatchette <hunterhatchette@outlook.com>
2018-12-02 17:58:39 -0500

    Account Manager Templating (EDIT)
    Template def's in Account Manager for later editing for actual results, still work in progress

--- AccountManager.py
+++ AccountManager.py
@@ -1,3 +1,5 @@
+# ----- Developer: Hunter Hatchette ----- #
+
 from flask import request, render_template, redirect
 from DB_Helper import DB_Helper
 
@@ -15,3 +17,104 @@ class AccountManager:
     def view_active_items(self, limit=1):
         db = DB_Helper
         sql = ()
+
+        data = (limit,)  # to satisfy execute method for prepared statement
+
+        cursor = db.connection.cursor(prepared=True)
+        cursor.execute(sql, data)
+        results = cursor.fetchall()
+
+        item_list = {'item': []}
+
+        for (ItemID, UserID, ClassID, Name, Image_Url, Status, Start_Bid, Current_Bid, Bid_Count, Start_Date, End_Date,
+             FirstName, LastName, Location) in results:
+            item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Start_Bid,
+                                      Current_Bid.decode(), Bid_Count, Start_Date, End_Date, FirstName, LastName,
+                                      Location])
+
+        db.disconnect()
+        return item_list
+
+    def view_sold_items(self, limit=1):
+        db = DB_Helper
+        sql = ()
+
+        data = (limit,)  # to satisfy execute method for prepared statement
+
+        cursor = db.connection.cursor(prepared=True)
+        cursor.execute(sql, data)
+        results = cursor.fetchall()
+
+        item_list = {'item': []}
+
+        for (ItemID, UserID, ClassID, Name, Image_Url, Status, Start_Bid, Current_Bid, Bid_Count, Start_Date, End_Date,
+             FirstName, LastName, Location) in results:
+            item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Start_Bid,
+                                      Current_Bid.decode(), Bid_Count, Start_Date, End_Date, FirstName, LastName,
+                                      Location])
+
+        db.disconnect()
+        return item_list
+
+    def view_watching_items(self, limit=1):
+        db = DB_Helper
+        sql = ()
+
+        data = (limit,)  # to satisfy execute method for prepared statement
+
+        cursor = db.connection.cursor(prepared=True)
+        cursor.execute(sql, data)
+        results = cursor.fetchall()
+
+        item_list = {'item': []}
+
+        for (ItemID, UserID, ClassID, Name, Image_Url, Status, Start_Bid, Current_Bid, Bid_Count, Start_Date, End_Date,
+             FirstName, LastName, Location) in results:
+            item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Start_Bid,
+                                      Current_Bid.decode(), Bid_Count, Start_Date, End_Date, FirstName, LastName,
+                                      Location])
+
+        db.disconnect()
+        return item_list
+
+    def view_bought_items(self, limit=1):
+        db = DB_Helper
+        sql = ()
+
+        data = (limit,)  # to satisfy execute method for prepared statement
+
+        cursor = db.connection.cursor(prepared=True)
+        cursor.execute(sql, data)
+        results = cursor.fetchall()
+
+        item_list = {'item': []}
+
+        for (ItemID, UserID, ClassID, Name, Image_Url, Status, Start_Bid, Current_Bid, Bid_Count, Start_Date, End_Date,
+             FirstName, LastName, Location) in results:
+            item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Start_Bid,
+                                      Current_Bid.decode(), Bid_Count, Start_Date, End_Date, FirstName, LastName,
+                                      Location])
+
+        db.disconnect()
+        return item_list
+
+    def view_bid_items(self, limit=1):
+        db = DB_Helper
+        sql = ()
+
+        data = (limit,)  # to satisfy execute method for prepared statement
+
+        cursor = db.connection.cursor(prepared=True)
+        cursor.execute(sql, data)
+        results = cursor.fetchall()
+
+        item_list = {'item': []}
+
+        for (ItemID, UserID, ClassID, Name, Image_Url, Status, Start_Bid, Current_Bid, Bid_Count, Start_Date, End_Date,
+             FirstName, LastName, Location) in results:
+            item_list['item'].append([ItemID, UserID, ClassID, Name.decode(), Image_Url.decode(), Status, Start_Bid,
+                                      Current_Bid.decode(), Bid_Count, Start_Date, End_Date, FirstName, LastName,
+                                      Location])
+
+        db.disconnect()
+        return item_list
