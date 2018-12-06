32624a1ef0c2a3fbc2c4c45374f21ff6030cc6e8
Christopher Torok <torok.chris@gmail.com>
2018-12-06 00:03:18 -0500

    attempt fix of deployment configuration

--- app_config.py
+++ app_config.py
@@ -14,12 +14,12 @@ import os
 
 def create_app(test_config=None):
     # create and configure the app
-    # app = Flask(__name__, instance_relative_config=True)
-    app = Flask(__name__)
-    # app.config.from_mapping(
-    #     SECRET_KEY=os.urandom(8),
-    #     # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
+    app = Flask(__name__, instance_relative_config=True)
+    # app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.urandom(8),
+        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
     # if test_config is None:
     #     # load the instance config, if it exists, when not testing
@@ -27,20 +27,15 @@ def create_app(test_config=None):
     # else:
     #     # load the test config if passed in
     #     app.config.from_mapping(test_config)
-    #
-    # # ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
 
-    # # a simple page that says hello
-    # @app.route('/hello')
-    # def hello():
-    #     return 'Hello, World!'
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     return app
 
 app = create_app()
-SECRET_KEY = os.urandom(8)
+# SECRET_KEY = os.urandom(8)
 # app.secret_key = os.urandom(8)
