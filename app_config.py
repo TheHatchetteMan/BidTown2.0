edb4cc276d2765b0a8bc02b8b8a4f31ac1014e69
Christopher Torok <torok.chris@gmail.com>
2018-12-06 00:10:46 -0500

    simplify app_config.py

--- app_config.py
+++ app_config.py
@@ -8,16 +8,15 @@ import os
 ######################################## APPLICATION CONFIGURATION ################################
 ###################################################################################################
 
-# app = Flask(__name__)  # flask object has underlying configuration capabilities.
-# app.secret_key = 'bidtown_session_key'  # set a key for session cookie signing.
-# app.secret_key = os.urandom(8)  # set a key for session cookie signing.
+app = Flask(__name__)  # flask object has underlying configuration capabilities.
+app.secret_key = 'bidtown_session_key'  # set a key for session cookie signing.
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-
-    return app
-
-app = create_app()
-# SECRET_KEY = os.urandom(8)
-app.secret_key = os.urandom(8)
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__)
+#
+#     return app
+#
+# app = create_app()
+# # SECRET_KEY = os.urandom(8)
+# app.secret_key = os.urandom(8)
